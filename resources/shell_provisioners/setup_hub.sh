ce0649420d2f44100bc8d2265d1b3ac7c29839af
Ted Zlatanov <tzz@lifelogs.com>
2013-07-17 14:36:26 -0400

    Hub setup adjustments

--- resources/shell_provisioners/setup_hub.sh
+++ resources/shell_provisioners/setup_hub.sh
@@ -3,4 +3,8 @@
 echo setting up MongoDB phpcfengine/usersettings
 
 /var/cfengine/bin/mongo phpcfengine --eval 'db.usersettings.insert({ "username" : "admin", "gitServer" : "/var/tmp/masterfiles.git", "gitEmail" : "root@thissystem.com", "gitAuthor" : "The Root Account", "gitBranch" : "master" })'
-install -m 0600 -o apache id_rsa /opt/cfengine/userworkdir/admin/.ssh/id_rsa.pvt
+install -d -m 0755 -o apache /opt/cfengine/userworkdir
+install -d -m 0755 -o apache /opt/cfengine/userworkdir/admin
+install -d -m 0755 -o apache /opt/cfengine/userworkdir/admin/.ssh
+install -m 0600 -o apache /vagrant/resources/shell_provisioners/id_rsa /opt/cfengine/userworkdir/admin/.ssh/id_rsa.pvt
+chown -R apache:apache /opt/cfengine/userworkdir
