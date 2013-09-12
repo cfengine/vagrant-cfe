5aca906b063f3dde4b995fb590f1cdabc182be31
Ted Zlatanov <tzz@lifelogs.com>
2013-09-12 16:19:40 -0400

    Added many new types; tested single_centos_source and single_ubuntu_source

--- resources/shell_provisioners/install_centos_core.sh
+++ resources/shell_provisioners/install_centos_core.sh
@@ -6,15 +6,17 @@ URL=https://github.com/cfengine/core.git
 test -d /var/cfengine && exit 0
 
 cd /var/tmp
-# yum install git autoconf libtool libtokyocabinet-dev libpcre3-dev libxml2-dev libvirt-dev libacl1-dev bison flex make libssl-dev
 
-# if [ -z "$1" ]; then
-#     git clone -q --depth 1 $URL ./core
-# else
-#     git clone -q --depth 1 --branch $1 $URL ./core
-# fi
+# half of these packages are not on various CentOS versions
+yum install -y git autoconf libtool libtokyocabinet-dev tokyocabinet-devel libpcre3-dev pcre-devel libxml2-dev libxml2-devel libvirt-dev libvirt-devel libacl1-dev libacl-devel bison flex make libssl-dev openssl-devel
 
-# cd ./core
-# ./autogen.sh
-# make
-# make install
+if [ -z "$1" ]; then
+    git clone -q --depth 1 $URL ./core
+else
+    git clone -q --depth 1 --branch $1 $URL ./core
+fi
+
+cd ./core
+./autogen.sh
+make
+make install
--- types.rb
+++ types.rb
@@ -21,24 +21,99 @@ module Types
     :ipackages => { :installer => :packages },
     :icore => { :installer => :core },
 
-    :ubuntu_10_04 => { :box => "ubuntu-10.04", :vmname => "BOX-" },
-    :ubuntu_13_04 => { :box => "ubuntu-13.04", :vmname => "BOX-" },
-    :centos_6_4 => { :box => "centos-6.4", :vmname => "BOX-" },
+    :ubuntu_10_04 => { :box => "ubuntu-10.04-amd64", :vmname => "BOX-64-" },
+    :ubuntu_10_04_i386 => { :box => "ubuntu-10.04-i386", :vmname => "BOX-32-" },
+    :ubuntu_10_10 => { :box => "ubuntu-10.10-amd64", :vmname => "BOX-64-" },
+    :ubuntu_11_04 => { :box => "ubuntu-11.04-amd64", :vmname => "BOX-64-" },
+    :ubuntu_11_10 => { :box => "ubuntu-11.10-amd64", :vmname => "BOX-64-" },
+    :ubuntu_12_04 => { :box => "ubuntu-12.04-amd64", :vmname => "BOX-64-" },
+    :ubuntu_12_10 => { :box => "ubuntu-12.10-amd64", :vmname => "BOX-64-" },
+    :ubuntu_13_04 => { :box => "ubuntu-13.04-amd64", :vmname => "BOX-64-" },
 
-    :centos_enterprise_rpm => { :install_version => "/vagrant/resources/packages/cfengine-nova-hub-3.5.1-1.x86_64.rpm" },
+    :debian_precise => { :box => "precise32", :vmname => "BOX-32-" },
+    :debian_lucid => { :box => "lucid32", :vmname => "BOX-32-" },
+
+    :centos_5_5 => { :box => "centos-5.5", :vmname => "BOX-64-" },
+    :centos_5_6 => { :box => "centos-5.6", :vmname => "BOX-64-" },
+    :centos_5_7 => { :box => "centos-5.7", :vmname => "BOX-64-" },
+    :centos_5_8 => { :box => "centos-5.8", :vmname => "BOX-64-" },
+    :centos_5_9 => { :box => "centos-5.9", :vmname => "BOX-64-" },
+    :centos_6_0 => { :box => "centos-6.0", :vmname => "BOX-64-" },
+    :centos_6_1 => { :box => "centos-6.1", :vmname => "BOX-64-" },
+    :centos_6_2 => { :box => "centos-6.2", :vmname => "BOX-64-" },
+    :centos_6_3 => { :box => "centos-6.3", :vmname => "BOX-64-" },
+    :centos_6_4 => { :box => "centos-6.4", :vmname => "BOX-64-" },
+
+    :core_checkout_351 => { :install_version => "3.5.1" },
+    :core_checkout_352 => { :install_version => "3.5.2" },
+    :core_checkout_master => { :install_version => "master" },
+
+    :centos_client_enterprise352_rpm => { :install_version => "/vagrant/resources/packages/cfengine-nova-3.5.2-1.x86_64.rpm" },
+    :centos_client_enterprise352_deb => { :install_version => "/vagrant/resources/packages/cfengine-nova_3.5.2-1_amd64.deb" },
+
+    :centos_hub_enterprise352_rpm => { :install_version => "/vagrant/resources/packages/cfengine-nova-hub-3.5.2-1.x86_64.rpm" },
+    :centos_hub_enterprise352_deb => { :install_version => "/vagrant/resources/packages/cfengine-nova-hub_3.5.2-1_amd64.deb" },
   }
 
   TEMPLATES = {
-    :hub => { :setup => ["bootstrap", "custom_masterfiles", "dc", "hub", "converge" ] },
+    :hub => { :setup => [ "bootstrap", "custom_masterfiles", "dc", "hub", "converge" ] },
+    :generic => { :setup => [ "converge" ] },
   }
 
   TYPES = {
     "single_centos_hub" => {}
       .merge(FRAGMENTS[:ipackages])
       .merge(FRAGMENTS[:centos_6_4])
-      .merge(FRAGMENTS[:centos_enterprise_rpm])
+      .merge(FRAGMENTS[:centos_hub_enterprise352_rpm])
       .merge(TEMPLATES[:hub])
-      .merge({ :ip => "10.0.0.20", :count => 1 })
+      .merge({ :ip => "10.0.0.20", :count => 1 }),
+
+    "single_ubuntu_hub" => {}
+      .merge(FRAGMENTS[:ipackages])
+      .merge(FRAGMENTS[:ubuntu_13_04])
+      .merge(FRAGMENTS[:centos_hub_enterprise352_deb])
+      .merge(TEMPLATES[:hub])
+      .merge({ :ip => "10.0.0.20", :count => 1 }),
+
+    "single_centos_eclient" => {}
+      .merge(FRAGMENTS[:ipackages])
+      .merge(FRAGMENTS[:centos_6_4])
+      .merge(TEMPLATES[:generic])
+      .merge({ :ip => "10.0.0.20", :count => 1 }),
+
+    "single_ubuntu_eclient" => {}
+      .merge(FRAGMENTS[:ipackages])
+      .merge(FRAGMENTS[:ubuntu_13_04])
+      .merge(FRAGMENTS[:centos_client_enterprise352_deb])
+      .merge(TEMPLATES[:generic])
+      .merge({ :ip => "10.0.0.20", :count => 1 }),
+
+    "single_centos_cclient" => {}
+      .merge(FRAGMENTS[:ipackages])
+      .merge(FRAGMENTS[:centos_6_4])
+      .merge(TEMPLATES[:generic])
+      .merge({ :ip => "10.0.0.20", :count => 1 }),
+
+    "single_ubuntu_cclient" => {}
+      .merge(FRAGMENTS[:ipackages])
+      .merge(FRAGMENTS[:ubuntu_13_04])
+      .merge(TEMPLATES[:generic])
+      .merge({ :ip => "10.0.0.20", :count => 1 }),
+
+    "single_centos_source" => {}
+      .merge(FRAGMENTS[:icore])
+      .merge(FRAGMENTS[:centos_6_4])
+      .merge(FRAGMENTS[:core_checkout_master])
+      .merge(TEMPLATES[:generic])
+      .merge({ :ip => "10.0.0.20", :count => 1 }),
+
+    "single_ubuntu_source" => {}
+      .merge(FRAGMENTS[:icore])
+      .merge(FRAGMENTS[:ubuntu_13_04])
+      .merge(FRAGMENTS[:core_checkout_master])
+      .merge(TEMPLATES[:generic])
+      .merge({ :ip => "10.0.0.20", :count => 1 }),
+
   }
 
   def Types.type(name)
