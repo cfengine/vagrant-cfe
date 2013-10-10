4ef0f71b3f57d2214b0206714e86e9922ecd6175
Ted Zlatanov <tzz@lifelogs.com>
2013-10-10 15:02:14 -0400

    jenkins plugin: add docs back

--- resources/plugins/jenkins-postbuild.sh
+++ resources/plugins/jenkins-postbuild.sh
@@ -17,4 +17,7 @@ echo "echo \"=BUILD_TIME=$NOW\"" >> $TMPFILE
 echo 'echo "+jenkins_module_loaded"' >> $TMPFILE
 chmod 700 $TMPFILE
 
+# This is the fun part: we use the feature.cf policy to set a persistent jenkinsnotify class (90 million minutes == very long time)
+# NOTE: this does NOT run the deployment policy!!!  It only sets a class which will be picked up in the next run
+
 sudo -n /var/cfengine/bin/cf-agent -I -f /var/cfengine/inputs/lib/3.5/feature.cf -b feature -Dfeature_set_90m_jenkinsnotify
